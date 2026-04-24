6847e71a55691091b58b9a6d06cbc363a9f7d556
Victor Alves <victoralvess2802@gmail.com>
2026-04-24 02:16:33 -0300

    Menu Latas e chamei funções do Estoque para Máquina

--- src/Maquina.py
+++ src/Maquina.py
@@ -6,6 +6,15 @@ class Maquina:
         self.estoque = Estoque()
         self.preco_lata = 10
         self.saldo = 0
+
+    def set_quantidade (self, bebida, quantidade):
+        self.estoque.set_quantidade(bebida, quantidade)
+
+    def get_quantidade(self, bebida):
+        return self.estoque.get_quantidade(bebida)
+
+    def add_quantidade(self,bebida,quantidade):
+        self.estoque.add_quantidade(bebida,quantidade)
     
 
 
